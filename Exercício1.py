2a0bcfa25758bde9141f23147af76af0c61ac877
Guilherme <gr0312@gmail.com>
2025-03-27 01:40:51 -0300

    Primeira Parte

---
+++ Exercício1.py
@@ -0,0 +1,12 @@
+'''Declarando os valores para a expressäo'''
+a= 10
+b= 20
+c= 3042
+d= 30
+e= 94
+f= 2
+g= 6
+h= 1
+'''Declarando uma variavel para a expressão'''
+x= a+b*c/d*(e+f)*g-h
+print(f"A resposta desta expressão é {x:,.0f}")
